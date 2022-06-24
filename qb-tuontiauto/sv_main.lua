be3e5397e979c4048f85352c2d3a09c8286cc024
Lazzis <79515120+Lazzisdeveloping@users.noreply.github.com>
2022-06-24 16:16:43 +0300

    Update sv_main.lua

--- qb-tuontiauto/sv_main.lua
+++ qb-tuontiauto/sv_main.lua
@@ -22,7 +22,7 @@ local pedit = {
 RegisterServerEvent('karpo_tuontiauto:myyauto')
 AddEventHandler('karpo_tuontiauto:myyauto', function(hinta)
 	local Player = QBCore.Functions.GetPlayer(source)
-    xPlayer.Functions.AddMoney('money', hinta)
+    Player.Functions.AddMoney('money', hinta)
 end)
 
 
